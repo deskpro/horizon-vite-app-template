82bde67e3f58bee36c46bf7032c49d838e2b9b83
Ashley Dawson <ashley.dawson@deskpro.com>
2021-12-15 10:45:32 +0000

    Added assets copy to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          hook: "writeBundle",
+          targets: [
+            { src: "./manifest.json", dest: "./dist/" },
+            { src: "./README.md", dest: "./dist/" },
+            { src: "./icon.svg", dest: "./dist/" },
+          ],
+        }),
+      ],
+    }
+  },
 });
